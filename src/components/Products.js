ac92f7aa74f2a9c200995880c9eee8bd643a2f0b
Bohdan Oskin <bogdanosckinss@gmail.com>
2023-03-18 18:00:22 +0200

    Added product container

---
+++ src/components/Products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Products() {
+    return (
+        <div className='py-10'>
+            <div className='flex flex-col items-center gap-4'>
+                <h1 className='text-2xl bg-black text-white py-2 w-80 text-center'>Shopping everyday!</h1>
+                <span className='w-20 h-[3px] bg-black'></span>
+                <p className='max-w-[700px] text-gray-600 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dignissimos ullam sint assumenda quod ipsum quia, consequatur recusandae sapiente cumque excepturi suscipit, atque labore? Alias numquam cum minima illum molestias?</p>
+            </div>
+        </div>
+    )
+}
+
+export default Products
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Banner from "../components/Banner";
+import Products from "../components/Products";
 
 const name = () => {
   return (
     <div>
       <Banner></Banner>
+      <Products></Products>
     </div>
   )
 };
